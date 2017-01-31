4e3d0f1bd642ac4c6dfe699886fc0f45ac190953
byYottaFLOPS <jonas.peeters@icloud.com>
2017-01-31 13:48:22 +0100

    Added Coin2 and Bill

--- init.lua
+++ init.lua
@@ -2,3 +2,12 @@ minetest.register_craftitem("physicalmoney:coin1", {
 	description = "1$ Coin",
 	inventory_image = "physicalmoney_coin1.png"
 })
+minetest.register_craftitem("physicalmoney:coin2", {
+	description = "2$ Coin",
+	inventory_image = "physicalmoney_coin2.png"
+})
+
+minetest.register_craftitem("physicalmoney:bill5", {
+	description = "5$ Bill",
+	inventory_image = "physicalmoney_bill5.png"
+})
