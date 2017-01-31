bb51ebad0498f9877faac72f077c25482c6563e2
byYottaFLOPS <jonas.peeters@icloud.com>
2017-01-31 14:00:28 +0100

    Added bills

--- init.lua
+++ init.lua
@@ -11,3 +11,27 @@ minetest.register_craftitem("physicalmoney:bill5", {
 	description = "5$ Bill",
 	inventory_image = "physicalmoney_bill5.png"
 })
+minetest.register_craftitem("physicalmoney:bill10", {
+	description = "10$ Bill",
+	inventory_image = "physicalmoney_bill10.png"
+})
+minetest.register_craftitem("physicalmoney:bill20", {
+	description = "20$ Bill",
+	inventory_image = "physicalmoney_bill20.png"
+})
+minetest.register_craftitem("physicalmoney:bill50", {
+	description = "50$ Bill",
+	inventory_image = "physicalmoney_bill50.png"
+})
+minetest.register_craftitem("physicalmoney:bill100", {
+	description = "100$ Bill",
+	inventory_image = "physicalmoney_bill100.png"
+})
+minetest.register_craftitem("physicalmoney:bill200", {
+	description = "200$ Bill",
+	inventory_image = "physicalmoney_bill200.png"
+})
+minetest.register_craftitem("physicalmoney:bill500", {
+	description = "500$ Bill",
+	inventory_image = "physicalmoney_bill500.png"
+})
